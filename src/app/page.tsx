5d2a61e846c02ac76a84fc4472dcc036a10b9970
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-30 10:06:01 +0100

    mise en place du téléchargement de fichier pdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+"use client"
 import Image from "next/image";
 
 
 export default function Home() {
 
- 
+  const PDF_FILE_URL = 'http://localhost:3000/file_pdf.pdf'
+
+  const dowloadFileAtURL = async (url: any) => {
+    const fileName = url.split("/").pop()
+    const aTag = document.createElement('a');
+    aTag.href = url
+    aTag.setAttribute("download", fileName)
+    document.body.appendChild(aTag)
+    aTag.click();
+    aTag.remove();
+  }
   return (
     <div className="App">
-      <button>Download File</button>
+      <button onClick={() => {dowloadFileAtURL(PDF_FILE_URL)}}>Download File</button>
     </div>
   );
 }
