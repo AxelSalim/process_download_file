d53abcdfc806366190f6a0d37e6df5dcb84a5200
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-30 18:25:21 +0100

    ajout du bouton pour le csv et le zip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import Image from "next/image";
 export default function Home() {
 
   const PDF_FILE_URL = 'http://localhost:3000/file_pdf.pdf'
+  const PNG_FILE_URL = 'http://localhost:3000/file_png.png'
+  const CSV_FILE_URL = 'http://localhost:3000/file_csv.xlsx'
+  const ZIP_FILE_URL = 'http://localhost:3000/file_zip.zip'
 
   const dowloadFileAtURL = async (url: any) => {
     const fileName = url.split("/").pop()
@@ -18,7 +21,9 @@ export default function Home() {
   return (
     <div className="App">
       <button onClick={() => {dowloadFileAtURL(PDF_FILE_URL)}}>Download File PDF</button>
-      <button onClick={() => {dowloadFileAtURL(PDF_FILE_URL)}}>Download File PNG</button>
+      <button onClick={() => {dowloadFileAtURL(PNG_FILE_URL)}}>Download File PNG</button>
+      <button onClick={() => {dowloadFileAtURL(CSV_FILE_URL)}}>Download File CSV</button>
+      <button onClick={() => {dowloadFileAtURL(ZIP_FILE_URL)}}>Download File ZIP</button>
     </div>
   );
 }
